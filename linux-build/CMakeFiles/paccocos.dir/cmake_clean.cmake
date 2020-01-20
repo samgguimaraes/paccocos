2f4730476897d957bf20ccdbda19bc5a5e7538ce
Samuel Gales Guimaraes <sg@spacetimeanalytics.com>
2020-01-20 00:05:58 -0300

    Empty project created

---
+++ linux-build/CMakeFiles/paccocos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paccocos.dir/Classes/AppDelegate.cpp.o"
+  "CMakeFiles/paccocos.dir/Classes/HelloWorldScene.cpp.o"
+  "CMakeFiles/paccocos.dir/proj.linux/main.cpp.o"
+  "bin/paccocos/paccocos"
+  "bin/paccocos/paccocos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paccocos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ linux-build/engine/external/unzip/CMakeFiles/ext_unzip.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sgg/dev/gms/paccocos/cocos2d/external/unzip/ioapi.cpp" "/home/sgg/dev/gms/paccocos/linux-build/engine/external/unzip/CMakeFiles/ext_unzip.dir/ioapi.cpp.o"
+  "/home/sgg/dev/gms/paccocos/cocos2d/external/unzip/ioapi_mem.cpp" "/home/sgg/dev/gms/paccocos/linux-build/engine/external/unzip/CMakeFiles/ext_unzip.dir/ioapi_mem.cpp.o"
+  "/home/sgg/dev/gms/paccocos/cocos2d/external/unzip/unzip.cpp" "/home/sgg/dev/gms/paccocos/linux-build/engine/external/unzip/CMakeFiles/ext_unzip.dir/unzip.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/GLFW"
+  "/usr/local/include/GLFW"
+  "../cocos2d/cocos/editor-support"
+  "../cocos2d/external/unzip/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
