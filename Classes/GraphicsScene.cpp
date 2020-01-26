86968f0ca2fbc53042eaddcf47227ac4464fb4a0
Samuel Gales Guimaraes <sg@spacetimeanalytics.com>
2020-01-25 23:17:31 -0300

    Experimenting
    Nothing really works... =[

--- Classes/GraphicsScene.cpp
+++ Classes/GraphicsScene.cpp
@@ -16,8 +16,29 @@ bool GraphicsScene::init()
     if (!Layer::init())
         return false;
 
-    auto sprite = Sprite::create("pacco.png");
-    sprite->setPosition(0, 0);
+
+//    Image imageWall;
+//    imageWall.initWithImageFile("walls.png");
+//    Texture2D* texWall;
+//    texWall->initWithImage(imageWall);
+
+    Vector<SpriteFrame*> frames;
+    frames.pushBack(SpriteFrame::create("pacco.png", Rect(0,0,32,32)));
+    frames.pushBack(SpriteFrame::create("pacco.png", Rect(0,32,32,32)));
+    frames.pushBack(SpriteFrame::create("pacco.png", Rect(32,0,32,32)));
+    frames.pushBack(SpriteFrame::create("pacco.png", Rect(32,32,32,32)));
+
+    Animation* animation = Animation::create();
+    animation->addSpriteFrame(frames.at(0));
+    animation->addSpriteFrame(frames.at(1));
+    animation->addSpriteFrame(frames.at(2));
+    animation->addSpriteFrame(frames.at(3));
+
+    auto sprite = Sprite::create();
+    sprite->setSpriteFrame(frames.at(0));
+    sprite->runAction(Animate::create(animation));
+    sprite->setPosition(100, 100);
+
 
     this->addChild(sprite, 0);
 
